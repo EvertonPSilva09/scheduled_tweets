2e7636d17fa4f4b569daf18415accd3e7bf4c626
Everton Silva <everton.p.silva@outlook.com>
2024-03-17 17:33:03 -0300

    feat: add new and create methods to SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "You have been logged in"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new, status: :unauthorized
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "You have been logged out"
