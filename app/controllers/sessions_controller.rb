0aae974554b03f965c4d559cee023ee282dfb526
Everton Silva <everton.p.silva@outlook.com>
2024-03-18 20:42:34 -0300

    change: update redirect path in destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "You have been logged out"
+    redirect_to sign_in_path, notice: "You have been logged out"
   end
 end
