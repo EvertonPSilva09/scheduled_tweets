ac26f82bf70d2cc9fd8c21575fc0d01642c8ff50
Everton Silva <everton.p.silva@outlook.com>
2024-03-18 21:11:32 -0300

    feat: add password validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  validates :password_confirmation, presence: true, if: -> { new_record? || !password.nil? }
 end
