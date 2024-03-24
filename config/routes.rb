51d424375e4efd6704f95a7d4152130be83cc80b
Everton Silva <everton.p.silva@outlook.com>
2024-03-23 23:56:25 -0300

    feat: add Sidekiq routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+require "sidekiq/web"
+Sidekiq::Web.app_url = "/"
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -19,7 +22,7 @@ Rails.application.routes.draw do
 
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
-  
+
   delete "logout", to: "sessions#destroy"
 
   get "auth/twitter/callback", to: "omniauth_callbacks#twitter"
@@ -28,4 +31,12 @@ Rails.application.routes.draw do
   resources :tweets
 
   root to: "main#index"
+
+  mount Sidekiq::Web => "/sidekiq"
+  get "job" => "job#index"
+  get "job/email" => "job#email"
+  get "job/post" => "job#delayed_post"
+  get "job/long" => "job#long"
+  get "job/crash" => "job#crash"
+  get "job/bulk" => "job#bulk"
 end
